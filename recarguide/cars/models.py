403a3e72e7b6620f65567ff29066a6125ec6e13d
resurtm <resurtm@gmail.com>
2017-10-12 13:23:11 +0600

    cars & common applications, cars' initial models

---
+++ recarguide/cars/models.py
@@ -0,0 +1,73 @@
+from django.db import models
+from django.db.models.signals import pre_save
+from django.utils.text import slugify
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=50, default='')
+    slug = models.SlugField(max_length=50, default='')
+
+    parent = models.ForeignKey('self', on_delete=models.CASCADE, null=True,
+                               default=None)
+
+    def __str__(self):
+        return '{name} {slug}'.format(name=self.name, slug=self.slug)
+
+
+class Make(models.Model):
+    name = models.CharField(max_length=50, default='')
+    slug = models.SlugField(max_length=50, default='')
+
+    def __str__(self):
+        return '{name} {slug}'.format(name=self.name, slug=self.slug)
+
+
+class Model(models.Model):
+    name = models.CharField(max_length=50, default='')
+    slug = models.SlugField(max_length=50, default='')
+
+    make = models.ForeignKey('Make', on_delete=models.CASCADE, default=None)
+
+    def __str__(self):
+        return '{name} {slug}'.format(name=self.name, slug=self.slug)
+
+
+class Car(models.Model):
+    name = models.SlugField(max_length=250, default='')
+    slug = models.SlugField(max_length=250, default='')
+
+    make = models.ForeignKey('Make', on_delete=models.PROTECT, default=None)
+    model = models.ForeignKey('Model', on_delete=models.PROTECT, default=None)
+    category = models.ForeignKey('Category', on_delete=models.PROTECT,
+                                 default=None)
+
+    price = models.PositiveIntegerField(default=0)
+    year = models.PositiveSmallIntegerField(default=0)
+
+    def __str__(self):
+        return '{name} {slug}'.format(name=self.name, slug=self.slug)
+
+
+def pre_save_category_receiver(instance):
+    instance.slug = slugify(instance.name)
+
+
+def pre_save_make_receiver(instance):
+    instance.slug = slugify(instance.name)
+
+
+def pre_save_model_receiver(instance):
+    instance.slug = slugify(instance.name)
+
+
+def pre_save_car_receiver(instance):
+    instance.slug = slugify(instance.name)
+    instance.name = '{make} {model} {year}'.format(make=instance.make.name,
+                                                   model=instance.model.name,
+                                                   year=instance.year)
+
+
+pre_save.connect(pre_save_category_receiver, sender=Category)
+pre_save.connect(pre_save_make_receiver, sender=Make)
+pre_save.connect(pre_save_model_receiver, sender=Model)
+pre_save.connect(pre_save_car_receiver, sender=Car)
--- recarguide/settings.py
+++ recarguide/settings.py
@@ -25,6 +25,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'recarguide.common.apps.CommonConfig',
+    'recarguide.cars.apps.CarsConfig',
 ]
 
 MIDDLEWARE = [
