65e0039ee062e3a17decd619c7e81b97835d1bfc
resurtm <resurtm@gmail.com>
2017-10-12 13:30:33 +0600

    single car page view

---
+++ recarguide/cars/urls.py
@@ -0,0 +1,7 @@
+from django.conf.urls import url
+
+from recarguide.cars import views
+
+urlpatterns = [
+    url(r'^(?P<slug>[0-9a-z_-]+)-(?P<id>\d+)$', views.view, name='view'),
+]
--- recarguide/cars/views.py
+++ recarguide/cars/views.py
@@ -1,3 +1,14 @@
+from django.http import Http404
 from django.shortcuts import render
 
-# Create your views here.
+from recarguide.cars.models import Car
+
+
+def view(request, slug, id):
+    try:
+        car = Car.objects.get(id=id)
+        if car.slug != slug:
+            raise Car.DoesNotExist()
+    except Car.DoesNotExist:
+        raise Http404('Requested car does not exist')
+    return render(request, 'cars/view.html', {'car': car})
