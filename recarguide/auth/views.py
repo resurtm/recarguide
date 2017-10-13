4866ffb9d7a9e1742be5f0fff38fb1173e5cd3cb
resurtm <resurtm@gmail.com>
2017-10-13 13:27:49 +0600

    finish sign up process

--- recarguide/auth/views.py
+++ recarguide/auth/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login
 
 from recarguide.auth.forms import UserCreationForm
 
@@ -7,7 +8,12 @@ def signup(request):
     if request.method == 'POST':
         form = UserCreationForm(request.POST)
         if form.is_valid():
-            pass
+            form.save()
+            user = authenticate(username=form.cleaned_data.get('username'),
+                                password=form.cleaned_data.get('password1'))
+            login(request, user)
+            # change to home page or something else
+            return redirect('auth:login')
     else:
         form = UserCreationForm()
     return render(request, 'registration/signup.html', {'form': form})
