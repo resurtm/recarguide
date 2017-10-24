b2472df19a25efb24dae0650e4a4c9f6ef858921
resurtm <resurtm@gmail.com>
2017-10-24 22:07:12 +0600

    travis ci

---
+++ recarguide/settings_local.sample.py
@@ -0,0 +1,33 @@
+# Change extension of this file from ".sample.py" to ".py" when using
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = ''
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'somedbname',
+        'USER': '',
+        'PASSWORD': '',
+        'HOST': '',
+        'PORT': '',
+        'TEST': {
+            'NAME': 'somedbname_test',
+        },
+    }
+}
+
+STRIPE = {
+    'PUBLISHABLE_KEY': 'pk_test_XXXXXX',
+    'SECRET_KEY': 'sk_test_XXXXXX',
+}
+
+CELERY_BROKER_URL = 'amqp://XXXXXX'
+
+AWS_S3_ACCESS_KEY_ID = 'AKIAXXXXXX'
+AWS_S3_SECRET_ACCESS_KEY = 'XXXXXX'
+AWS_S3_REGION_NAME = 'eu-central-1'
+AWS_S3_BUCKET_NAME = 'XXXXXX'
--- recarguide/settings_local.tests.py
+++ recarguide/settings_local.tests.py
@@ -1,7 +1,7 @@
-# Change extension of this file from ".sample" to ".py" when using
+# Change extension of this file from ".sample.py" to ".py" when using
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = ''
+SECRET_KEY = 's3cret$tr1ng123456'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -9,13 +9,13 @@ DEBUG = True
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'somedbname',
-        'USER': '',
-        'PASSWORD': '',
-        'HOST': '',
-        'PORT': '',
+        'NAME': 'testdb',
+        'USER': 'testuser',
+        'PASSWORD': 't3stpa$$w0rd',
+        'HOST': 'localhost',
+        'PORT': '5432',
         'TEST': {
-            'NAME': 'somedbname_test',
+            'NAME': 'testdb',
         },
     }
 }
