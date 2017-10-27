9f5f6bacf132357a35839fe54902746692d750e7
resurtm <resurtm@gmail.com>
2017-10-27 22:16:09 +0600

    elastic search wip

---
+++ recarguide/cars/templatetags/current_search.py
@@ -0,0 +1,15 @@
+from django import template
+
+from recarguide.cars.search.faceted import PARAMS
+
+register = template.Library()
+
+
+@register.inclusion_tag('cars/current_search.html')
+def current_search(fs):
+    params = []
+    for title, id, key, __ in PARAMS:
+        value = getattr(fs, id)
+        if value:
+            params.append((title, '#', value))
+    return {'params': params}
--- recarguide/cars/tests/search_faceted/faceted_search_params.py
+++ recarguide/cars/tests/search_faceted/faceted_search_params.py
@@ -6,8 +6,8 @@ from recarguide.cars.search.faceted import FacetedSearch as FS
 class FacetedSearchParamsTestCase(TestCase):
     def test_case1(self):
         params = {'keyword': None, 'make': None, 'model': None,
-                  'category': None, 'year': None, 'price': None,
-                  'has_picture': None, 'sold_listings': None}
+                  'category': None, 'subcategory': None, 'year': None,
+                  'price': None, 'has_picture': None, 'sold_listings': None}
 
         fs = FS()
         self.assertEqual(fs.params, params)
@@ -17,10 +17,10 @@ class FacetedSearchParamsTestCase(TestCase):
 
     def test_case2(self):
         fs = FS({'q': 'test1', 'm': 'test2', 'n': 'test3',
-                 'c': 'test4', 'y': '2500-7500', 'p': '5550-8650',
-                 'hp': '1', 'sl': '1'})
+                 'c': 'test4', 's': 'test5', 'y': '2500-7500',
+                 'p': '5550-8650', 'hp': '1', 'sl': '1'})
         params = {'keyword': 'test1', 'make': 'test2', 'model': 'test3',
-                  'category': 'test4', 'year': (2500, 7500),
-                  'price': (5550, 8650), 'has_picture': True,
-                  'sold_listings': True}
+                  'category': 'test4', 'subcategory': 'test5',
+                  'year': (2500, 7500), 'price': (5550, 8650),
+                  'has_picture': True, 'sold_listings': True}
         self.assertEqual(fs.params, params)
